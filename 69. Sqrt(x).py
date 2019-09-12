7c6dd55bfd6d9ff150b241bc44627088aa858739
huijuan <huijuan1991@hotmail.com>
2019-09-11 17:02:39 -0700

    add

---
+++ 69. Sqrt(x).py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        start = 0
+        end = x
+        while start <= end:
+            mid = start + (end - start)/2
+
+            if mid*mid == x:
+                return mid
+            if mid*mid>x and (mid-1)*(mid-1)<x:
+                return mid - 1
+            elif mid*mid < x:
+                start = mid + 1
+            else:
+                end = mid - 1
+
+
+
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        left, right = 0, x
+        #mid = -1
+        if x < 2:
+            return x
+        while left < right:
+            mid = (left+right)/2
+            if x/mid >= mid:
+                left = mid+1
+            else:
+                right = mid
+            #print mid
+        return right - 1
