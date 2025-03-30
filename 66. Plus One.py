f13b898d8ea7edbb24bbf1c72012466370312f59
Joan P <joanmacair@Joans-MacBook-Air.local>
2025-03-29 22:56:31 -0700

    0329

--- 66. Plus One.py
+++ 66. Plus One.py
@@ -4,14 +4,27 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        carry = 1
-        i = len(digits)-1
-        while i >= 0:
-            carry += digits[i]
-            digits[i] = carry%10
-            carry = carry//10
-            i-=1
-        if carry:
-            digits[0] += 1
-            digits.append(0)
+        # i = len(digits) - 1
+        # carry = 0
+        # while i >= 0:
+        #     carry += digits[i]
+        #     digits[i] = carry % 10
+        #     carry =  carry/10
+        #     i -= 1
+        # if carry:
+        #     #999 + 1 = 100 + 0
+        #     digits[0] += 1
+        #     digits.append(0)
+        # return digits
+
+        # solution2: 
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] = digits[i] + 1
+                return digits
+        # return [1] + digits
+        digits.insert(0,1)
         return digits
+
--- 9. Palindrome Number.py
+++ 9. Palindrome Number.py
@@ -4,11 +4,22 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
+        # solution 1：Compare entire string, 正反一致
+        # if x < 0:
+        #     return False
+        # else:
+        #     x = str(x)
+        #     if x == x[::-1]:
+        #         return True
+        #     else:
+        #         return False
+
+        # Solution 2: 不转成string
         if x < 0:
             return False
-        else:
-            x = str(x)
-            if x == x[::-1]:
-                return True
-            else:
-                return False
+        newNum = 0
+        inputNum = x
+        while x > 0:
+            newNum = newNum * 10 + x % 10
+            x //= 10
+        return newNum == inputNum
